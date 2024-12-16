1a30e2de47803f5ab247926dcb4a781406cf6b30
Kaen147 <ddeexxttiirr@gmail.com>
2024-12-16 14:55:56 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
 }
+
+// Get the menu icon and navbar
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+
+// Toggle the 'active' class on the navbar when the menu icon is clicked
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
